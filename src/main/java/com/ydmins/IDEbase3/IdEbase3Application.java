1e15415a9d7cae8c40ee044b987aa4e534a944a5
ydmins <dr.ydmins@gmail.com>
2023-09-08 14:52:41 +0900

    first init

---
+++ src/main/java/com/ydmins/IDEbase3/IdEbase3Application.java
@@ -0,0 +1,13 @@
+package com.ydmins.IDEbase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdEbase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdEbase3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydmins/IDEbase3/IdEbase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.IDEbase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdEbase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
